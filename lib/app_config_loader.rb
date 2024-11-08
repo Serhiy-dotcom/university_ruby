c50e733bec96ee894ad7999e47e9622f28a6a9b2
DimaAdzh <dimaadzubej49@gmail.com>
2024-11-08 22:10:19 +0200

    lab-1.2

---
+++ lib/app_config_loader.rb
@@ -0,0 +1,51 @@
+require 'yaml'
+require 'erb'
+require 'json'
+
+module LakustaApplication
+  class AppConfigLoader
+	def load_libs
+	  system_libs = ['date']
+	  system_libs.each { |lib| require lib }
+	  
+	  # Підключаємо всі Ruby файли в `lib`, крім самого `app_config_loader.rb`
+	  Dir.glob(File.expand_path('../*.rb', __dir__)).each do |file|
+		require file unless file == __FILE__
+	  end
+	end
+
+	def create_product_catalog
+	  Dir.mkdir('config/products') unless Dir.exist?('config/products')
+	end
+
+    def initialize(config_file, yaml_dir)
+      @config_file = config_file
+      @yaml_dir = yaml_dir
+      @config_data = {}
+    end
+
+    def config
+      load_default_config
+      load_additional_configs
+      @config_data
+    end
+
+    def pretty_print_config_data
+      puts JSON.pretty_generate(@config_data)
+    end
+
+    private
+
+    def load_default_config
+	  erb_content = ERB.new(File.read(@config_file)).result(binding)
+	  @config_data.merge!(YAML.safe_load(erb_content))
+	end
+
+    def load_additional_configs
+      Dir.glob(File.join(@yaml_dir, '*.yaml')).each do |file|
+        yaml_content = YAML.safe_load(File.read(file))
+        @config_data.merge!(yaml_content)
+      end
+    end
+  end
+end
--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,27 @@
-require_relative "scraper"
+require_relative 'app_config_loader'
+require_relative 'logger_manager'
 
-url = "https://www.wikipedia.com.ua"
-scraper = Scraper.new(url)
+module LakustaApplication
+	class Main
+	  def self.start
+		config_loader = AppConfigLoader.new('config/default_config.yaml', 'config')
+		puts "Підключення бібліотек..."
+		config_loader.load_libs
+		puts "Бібліотеки підключено успішно."
 
-puts "Заголовки на сторінці:"
-scraper.extract_headings.each do |heading|
-  puts heading
+		puts "Завантаження конфігурацій..."
+		config_data = config_loader.config
+		puts "Конфігурації завантажено успішно."
+
+		puts "Перевірка завантаження конфігурацій:"
+		config_loader.pretty_print_config_data
+
+		LoggerManager.initialize_logger(config_data)
+		LoggerManager.log_processed_file('app.log')
+		LoggerManager.log_error('Log error')
+		puts "Логування налаштовано. Перевірте app.log для записаних подій."
+	  end
+	end
 end
+
+LakustaApplication::Main.start
